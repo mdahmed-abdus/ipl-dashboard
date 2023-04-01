bcc1e3610ca098f1df595ad103e55eff9e4c001d
Mohd Ahmed Abdus <mdahmed.abdus@gmail.com>
2023-04-01 12:36:46 +0530

    Add Team API endpoint

---
+++ src/main/java/com/mdahmed/ipldashboard/data/MatchDataProcessor.java
@@ -0,0 +1,48 @@
+package com.mdahmed.ipldashboard.data;
+
+
+import com.mdahmed.ipldashboard.model.Match;
+import org.springframework.batch.item.ItemProcessor;
+
+import java.time.LocalDate;
+
+public class MatchDataProcessor implements ItemProcessor<MatchInput, Match> {
+
+    @Override
+    public Match process(final MatchInput matchInput) {
+        Match match = new Match();
+
+        match.setId(Long.parseLong(matchInput.getId()));
+        match.setCity(matchInput.getCity());
+        match.setDate(LocalDate.parse(matchInput.getDate()));
+        match.setPlayerOfMatch(matchInput.getPlayer_of_match());
+        match.setVenue(matchInput.getVenue());
+
+//        set team 1 and team 2 depending on the inning order
+        String firstInningsTeam, secondInningsTeam;
+
+        if ("bat".equals(matchInput.getToss_decision())) {
+            firstInningsTeam = matchInput.getToss_winner();
+            secondInningsTeam = matchInput.getToss_winner().equals(matchInput.getTeam1())
+                    ? matchInput.getTeam2() : matchInput.getTeam1();
+        } else {
+            secondInningsTeam = matchInput.getToss_winner();
+            firstInningsTeam = matchInput.getToss_winner().equals(matchInput.getTeam1())
+                    ? matchInput.getTeam2() : matchInput.getTeam1();
+        }
+
+        match.setTeam1(firstInningsTeam);
+        match.setTeam2(secondInningsTeam);
+
+        match.setTossDecision(matchInput.getToss_decision());
+        match.setTossWinner(matchInput.getToss_winner());
+        match.setMatchWinner(matchInput.getWinner());
+        match.setResult(matchInput.getResult());
+        match.setResultMargin(matchInput.getResult_margin());
+        match.setUmpire1(matchInput.getUmpire1());
+        match.setUmpire2(matchInput.getUmpire2());
+
+        return match;
+    }
+
+}
---
+++ src/main/java/com/mdahmed/ipldashboard/repository/TeamRepository.java
@@ -0,0 +1,10 @@
+package com.mdahmed.ipldashboard.repository;
+
+import com.mdahmed.ipldashboard.model.Team;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TeamRepository extends CrudRepository<Team, Long> {
+
+    Team findByTeamName(String teamName);
+
+}
